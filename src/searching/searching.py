269c1be8b6ffffa5dad1d5866d6b7be1548e6538
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-08-09 23:31:16 +0800

    completed binary_search, completed MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -10,6 +10,20 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
     # Your code here
+    first = 0
+    last = (len(arr) - 1)
+    found = False
 
+    while first <= last and not found: 
+        middle = (first + last) // 2
+
+        if arr[middle] == target:
+            found = True
+            return middle
+        else:
+            if target < arr[middle]:
+                last = middle - 1
+            else:
+                first = middle + 1
 
     return -1  # not found
